43357237e63bd42d11cd5047a81acbb9a2383f76
Jim <qzy114@gmail.com>
2021-07-07 15:51:55 -0400

    first commit

---
+++ src/test/java/com/jim/springbootnew/SpringbootnewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jim.springbootnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootnewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
