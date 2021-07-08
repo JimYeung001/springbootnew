8c8b1e78e7d412854bd06008d91cafada636eba5
Jim <qzy114@gmail.com>
2021-07-07 20:23:45 -0400

    added security

--- src/main/java/com/jim/springbootnew/resource/PersonResource.java
+++ src/main/java/com/jim/springbootnew/resource/PersonResource.java
@@ -3,21 +3,23 @@ package com.jim.springbootnew.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jim.springbootnew.model.Person;
 import com.jim.springbootnew.repo.PersonRepository;
 
 @RestController
-@RequestMapping("/api/people")
+@RequestMapping(value ="/api", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 public class PersonResource {
 	
 	@Autowired
 	private PersonRepository personRepo;
 	
-	@GetMapping("/all")
+	@GetMapping("/people")
 	public List<Person> getAllPeople(){
 		return getPersonRepo().findAll();
 	}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO tb_person(first_name, last_name, age) VALUES ( 'Jim', 'Yeung', 20);
-INSERT INTO tb_person(first_name, last_name, age) VALUES ( 'Joy', 'Yeung', 20);
-INSERT INTO tb_person(first_name, last_name, age) VALUES ( 'Emy', 'Yeung', 20);
+INSERT INTO tb_person(first_name, last_name, age) VALUES ( 'Jim', 'Yeung', 90);
+INSERT INTO tb_person(first_name, last_name, age) VALUES ( 'Joy', 'Yeung', 70);
+INSERT INTO tb_person(first_name, last_name, age) VALUES ( 'Emy', 'Yeung', 50);
 INSERT INTO tb_person(first_name, last_name, age) VALUES ( 'Tom', 'Jerry', 20);
